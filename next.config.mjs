aa0bec0dd685c7d19cdfec3b244530b373f12cb9
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2026-02-05 18:27:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows images from Supabase Storage
+      },
+    ],
+  },
+};
 
+export default nextConfig;
