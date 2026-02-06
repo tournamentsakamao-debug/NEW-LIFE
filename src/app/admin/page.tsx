f2c3d6ff0997fdbbc7f05b62b36b267718ee1638
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2026-02-06 21:24:33 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '@/lib/supabase';
 import { AdminGuard } from '../../lib/authGuard';
 import { motion } from 'framer-motion';
 
