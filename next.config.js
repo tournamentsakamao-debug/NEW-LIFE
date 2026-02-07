6b4f6c83cb782f85492aa63b4922bda06dd82c11
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2026-02-07 09:18:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['pcgzjgfzcyucvwhrldbq.supabase.co'],
+    unoptimized: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      }
+    }
+    return config
   },
   experimental: {
     serverActions: true,
-  },
+  }
 }
+
+module.exports = nextConfig
